46b2b70f47342cbdc458b53fd766a2be141c5c00
Ybadoo <106773882+Ybadoo@users.noreply.github.com>
2024-04-10 16:07:07 -0300

    Update Fatorial.c
    Inclusão do enunciado

--- teoria-computacao/programas-iterativos/01/Fatorial.c
+++ teoria-computacao/programas-iterativos/01/Fatorial.c
@@ -1,3 +1,12 @@
+/*
+ * Desenvolver um programa iterativo, sobre uma máquina genérica, que apresente o fatorial de n utilizando a fórmula f = 1 * 2 * 3 * 4 * ... * n. 
+ * O valor de n será fornecido pelo usuário, devendo ser um valor inteiro maior ou igual a zero.
+ * Por exemplo, caso o valor fornecido pelo usuário para n seja 5, o programa deverá apresentar como resposta o valor 120, ou seja, 1 * 2 * 3 * 4 * 5.
+ * Caso o usuário forneça um valor inválido para n, o programa deverá apresentar como resposta o valor -1.
+ *
+ * www.ybadoo.com.br/tutoriais/teoria-computacao/programas-iterativos/01/
+ */
+
 #include <stdio.h>
 
 int main ()
